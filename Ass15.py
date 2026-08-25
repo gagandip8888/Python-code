e855684bf171acf413a62c141ef7063a491c8e03
gagandip8888 <gaganit8888@gmail.com>
2026-08-25 18:07:44 +0530

    Add Python daily assignments

--- Ass15.py
+++ Ass15.py
@@ -1,2 +1,109 @@
 # 1. Student Class – __str__() Create a Student class with name, roll_no, and marks. 
 # Implement __str__() so that printing the 
+
+class Student:
+   def __init__(self,name,roll_no,marks):
+      self.name=name
+      self.roll_no=roll_no
+      self.marks=marks
+
+   def __str__(self):
+       return f"Name:{self.name},Roll no{self.roll_no},marks{self.marks}"
+   
+student1=Student("sarthak", 88, 76)
+print(student1)
+
+# 2. Product Class – __eq__() Create a Product class with name and price. Implement __eq__() 
+# to check whether two products have the same price. 
+
+class Product:
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+
+    def __eq__(self, value):
+        return self.price==value.price
+    
+product1=Product("pen",20)
+product2=Product("pencil",20)
+
+print(product1==product2)
+   
+   
+   
+# 3. Shopping Cart – __len__() Create a ShoppingCart class containing a list of products. 
+# Implement __len__() so that len(cart) returns the number of products in the cart. 
+
+class shoping:
+    def __init__(self,product):
+        self.product=product
+    
+    def __len__(self):
+        return len(self.product)
+    
+product=shoping("sdfghjk")
+print(len(product))
+
+
+# 4. Bank Account – __str__() Create a BankAccount class with account holder name, account number,
+# and balance. Implement __str__() to display account information when the object is printed.
+
+class BankAccount:
+    def __init__(self,name,number,balance):
+        self.name=name
+        self.number=number
+        self.balance=balance
+
+    def __str__(self):
+        return f"Account Holder Name{self.name},Account Number{self.number},Balance{self.balance}"
+    
+information=BankAccount("Gagandip pathare",1247500332,5000)
+print(information)
+
+
+# 5. Number Class – __add__() Create a Number class that stores one number.
+# Implement __add__() so that two objects can be added 
+
+class Number:
+    def __init__(self,number):
+        self.number=number
+
+    def __add__(self, other):
+        return self.number +other.number
+    
+num1=Number(20)
+num2=Number(20)
+
+print(num1+num2)
+
+
+# 6. Create a Number class and implement: __add__()  __sub__()  
+# __mul__()  __truediv__()  __mod__() 
+        
+        
+class Number:
+    def __init__(self,number):
+        self.number=number
+
+    def __add__(self, other):
+        return self.number+other.number
+    def __sub__(self, other):
+        return self.number - other.number
+    
+    def __mul__(self, other):
+        return self.number*other.number
+    
+    def __truediv__(self, other):
+        return self.number/other.number
+    
+    def __mod__(self, other):
+        return self.number% other.number
+    
+num1 = Number(20)
+num2 = Number(10)
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1%num2)
--- Ass16.py
+++ Ass16.py
@@ -1,2 +1,259 @@
 # 1. Create a Person parent class with name and age. Create a Student child class with roll_no and marks. 
 # Use super() to initialize the parent attributes. 
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+class Student(Person):
+    def __init__(self,name,age,roll_no,marks):
+        super().__init__(name,age)
+        self.rollno=roll_no
+        self.marks=marks
+        
+student1=Student("Gagandip",22,88,78)
+print(student1.name)
+print(student1.age)
+print(student1.rollno)
+print(student1.marks)
+
+print()
+# 2. Create a Vehicle parent class with a start() method. Create Car and Bike child classes and
+# override the start() method with different behavior. 
+
+class Vehicle:
+    def Start(self):
+        print("car is start")
+        
+class Car (Vehicle):
+    def Start(self):
+        print("car start with key")
+        
+class Bike(Vehicle):
+    def Start(self):
+        print("bike start ")
+        
+car=Car()
+bike=Bike()
+car.Start()
+bike.Start()
+
+
+# 3. Create an Employee parent class with name and salary. Create a Manager child class with an 
+# additional department attribute. Display all details using constructors. 
+
+class Employee:
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+
+class Manager(Employee):
+    def __init__(self, name, salary,department):
+        super().__init__(name, salary)
+        self.department=department
+
+manager1=Manager("sarthak",50000,"Hr")
+print(manager1.name)
+print(manager1.salary)
+print(manager1.department)
+
+
+# 4. Create a BankAccount parent class with account_number and balance. Create a SavingsAccount child class 
+# with an additional interest_rate. Use super() in the child constructor. 
+
+class BankAccount:
+    def __init__(self,account_number,balance):
+        self.account=account_number
+        self.balance=balance
+
+class SavingAccount(BankAccount):
+    def __init__(self, account_number, balance,intrest_rate):
+        super().__init__(account_number, balance)
+        self.intrest=intrest_rate
+        
+bankaccount=SavingAccount(1247500332,850,5)
+print(bankaccount.account)
+print(bankaccount.balance)
+print(bankaccount.intrest)
+
+# 5. Create a Person parent class with a display() method. Create a Student child class that overrides 
+# display() but also calls the parent display() using super()
+
+class Company:
+
+    def __init__(self, company_name):
+        self.company = company_name
+
+class Developer(Company):
+
+    def __init__(self, company_name, programming_lang):
+        super().__init__(company_name)
+        self.programming = programming_lang
+
+    def coding(self):
+        print("coding")
+
+class Tester(Company):
+
+    def __init__(self, company_name, testing_tool):
+        super().__init__(company_name)
+        self.testing_tool = testing_tool
+
+    def testing(self):
+        print("testing")
+developer1 = Developer("Infosys", "Python")
+tester1 = Tester("Infosys", "Playwright")
+
+print(developer1.company)
+print(developer1.programming)
+developer1.coding()
+
+print(tester1.company)
+print(tester1.testing_tool)
+tester1.testing()
+
+
+# 7. Bank Account
+# Create a BankAccount class with a private attribute __balance. 
+# Create get_balance() to view the balance. 
+# Create set_balance() to update the balance only if the amount is valid.
+# Create a withdraw() method with proper balance validation. 
+
+class BankAccount:
+
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def get_balance(self):
+        return self.__balance
+
+    def set_balance(self, amount):
+        if amount >= 0:
+            self.__balance = amount
+        else:
+            print("Invalid amount")
+            
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance = self.__balance - amount
+            print("Withdraw ")
+        else:
+            print("no  balance in account")
+
+account = BankAccount(5000)
+print(account.get_balance())
+account.set_balance(7000)
+print(account.get_balance())
+account.withdraw(2000)
+print(account.get_balance())
+account.withdraw(6000)
+
+
+# 8. Student Marks
+# Create a Student class with private attributes __name and __marks. 
+# Create getter and setter methods for marks.  
+# Allow marks only between 0 and 100.  
+# Create a child class ExamStudent that inherits from Student 
+
+class Student:
+
+    def __init__(self, name, marks):
+        self.__name = name
+        self.__marks = marks
+
+    def get_marks(self):
+        return self.__marks
+
+    def set_marks(self, marks):
+        if marks >= 0 and marks <= 100:
+            self.__marks = marks
+        else:
+            print("no marks")
+
+
+class ExamStudent(Student):
+
+    def __init__(self, name, marks):
+        super().__init__(name, marks)
+
+
+student1 = ExamStudent("Gagan", 85)
+
+print(student1.get_marks())
+student1.set_marks(95)
+print(student1.get_marks())
+
+student1.set_marks(120)
+
+
+# 9. Constructor + Private Data Create a User class with private __password. 
+# Initialize it through the constructor.  
+# Create get_password() and set_password().  
+# Setter should update the password only if it contains at least 6 characters.
+
+class User:
+
+    def __init__(self, password):
+        self.__password = password
+
+    def get_password(self):
+        return self.__password
+
+    def set_password(self, password):
+        if len(password) >= 6:
+            self.__password = password
+        else:
+            print("Invalid password")
+
+
+user1 = User("gagan123")
+
+print(user1.get_password())
+
+user1.set_password("python123")
+print(user1.get_password())
+user1.set_password("abc") 
+
+
+# 10. Multilevel Inheritance + Getter/Setter
+# Person → private age with getter/setter.  
+# Employee → salary.  Manager → department.  Use super() at each level 
+
+class Person:
+
+    def __init__(self, age):
+        self.__age = age
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, age):
+        if age > 0:
+            self.__age = age
+        else:
+            print("Invalid age")
+
+
+class Employee(Person):
+
+    def __init__(self, age, salary):
+        super().__init__(age)
+        self.salary = salary
+
+
+class Manager(Employee):
+
+    def __init__(self, age, salary, department):
+        super().__init__(age, salary)
+        self.department = department
+
+
+manager1 = Manager(25, 50000, "IT")
+
+print(manager1.get_age())
+print(manager1.salary)
+print(manager1.department)
+
+manager1.set_age(30)
+print(manager1.get_age())
